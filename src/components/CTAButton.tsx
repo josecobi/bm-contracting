903041bf50b759fec30bfd356cf1610cc4e8dfcd
josecobi <cobitremolo@gmail.com>
2025-06-23 14:48:35 -0400

    created call to action button

---
+++ src/components/CTAButton.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Link from 'next/link'
+
+export default function CTAButton() {
+  return (
+    <Link href="/contact">
+      <button className="bg-yellow-500 text-gray-900 px-6 py-3 rounded-full font-semibold shadow-lg hover:bg-yellow-400 transition">
+        Get in Touch
+      </button>
+    </Link>
+  )
+}

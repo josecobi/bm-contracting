1ad770cf99d2c3200a5ef6c906c7da6701b2eb34
josecobi <cobitremolo@gmail.com>
2025-06-23 14:47:37 -0400

    created portfolio page

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,11 @@
+import BeforeAfterShowcase from '@/app/portfolio/components/BeforeAfter'
+
+export default function PortfolioPage() {
+  return (
+    <main>
+      {/* Other sections */}
+      <BeforeAfterShowcase />
+      {/* CTA or footer */}
+    </main>
+  )
+}
